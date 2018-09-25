9a918425cf68be817f2c50f3ab9674d8e47b9d59
yhw0709 <37795552+yhw0709@users.noreply.github.com>
2018-09-25 15:45:08 +0800

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog',
         'USER': 'root',
-        'PASSWORD': 'Yhw@960709',
+        'PASSWORD': '******',
         'HOST': '127.0.0.1',
         'PORT': 3306,
     }
